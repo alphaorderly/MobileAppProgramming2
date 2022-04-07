147f5081fe3bd43528932ac6a0efc4f0c468deff
Woo Kyeong Hoon <ilov1112@gmail.com>
2022-04-07 15:12:28 +0900

    Initial Commit
    make project

---
+++ MobileAppProject/MobileAppProject/MobileAppProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileAppProjectApp.swift
+//  MobileAppProject
+//
+//  Created by WOO on 2022/04/07.
+//
+
+import SwiftUI
+
+@main
+struct MobileAppProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
