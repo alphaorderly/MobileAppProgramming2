a35858cf64c2ec101e3c182f39e0a61acaec8354
Woo Kyeong Hoon <ilov1112@gmail.com>
2022-04-11 20:42:26 +0900

    검색창 만듦
    메인화면에 검색창

--- GoRightNow/GoRightNow/ContentView.swift
+++ GoRightNow/GoRightNow/ContentView.swift
@@ -9,13 +9,57 @@ import SwiftUI
 import Alamofire
 
 struct ContentView: View {
+    @State var searchText: String
     var body: some View {
-        Text("hello gaja")
+        VStack {
+            SearchBar(textInput: $searchText)
+            Spacer()
+            Text(searchText)
+        }
+    }
+}
+
+struct SearchBar: View {
+    @Binding var textInput: String
+    @State var editText: Bool = false
+    
+    var body: some View {
+        HStack {
+            TextField("국가명을 입력해주세요", text: self.$textInput)
+                .padding(15)
+                .padding(.horizontal, 15)
+                .background(Color(.systemGray6))
+                .cornerRadius(15)
+                .overlay(
+                    HStack {
+                        Spacer()
+                        if self.editText {
+                            Button(action: {
+                                self.editText = false
+                                self.textInput = ""
+                                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                            }){
+                                Image(systemName: "multiply.circle.fill")
+                                    .foregroundColor(Color(.black))
+                                    .padding()
+                            }
+                        } else {
+                            Image(systemName: "magnifyingglass")
+                                .foregroundColor(Color(.black))
+                                .padding()
+                        }
+                    }
+                )
+                .onTapGesture {
+                    self.editText = true
+                }
+        }
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(searchText: "")
     }
 }
