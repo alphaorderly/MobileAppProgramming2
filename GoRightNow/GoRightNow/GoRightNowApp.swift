bc2b196968b3c84e3f5a6bfd63acf7008eb613a3
Woo Kyeong Hoon <ilov1112@gmail.com>
2022-04-11 22:08:05 +0900

    사이드 메뉴 추가

--- GoRightNow/GoRightNow/GoRightNowApp.swift
+++ GoRightNow/GoRightNow/GoRightNowApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct GoRightNowApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView(searchText: "")
+            ContentView()
         }
     }
 }
