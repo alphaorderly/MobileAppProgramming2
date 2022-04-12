7a24e238f91efde2bad91a41074c9456abdc2ac8
Woo Kyeong Hoon <ilov1112@gmail.com>
2022-04-12 15:15:29 +0900

    MVVM 구조 채택
    MVVM 구조

---
+++ GoRightNow/GoRightNow/GoRightNowModel.swift
@@ -0,0 +1,14 @@
+//
+//  GoRightNowModel.swift
+//  GoRightNow
+//
+//  Created by WOO on 2022/04/12.
+//
+
+import Foundation
+import SwiftUI
+
+struct GoRightNowModel {
+    var textInput: String = "";
+    var sideMenu: Bool = false
+}
