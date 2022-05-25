bc64de77a3561dde317eafabd5b4d6cb27901c4b
Woo Kyeong Hoon <ilov1112@gmail.com>
2022-05-25 15:12:58 +0900

    플래너 디테일 뷰 준비

---
+++ GoRightNow/GoRightNow/Planner/PlannerDetailView.swift
@@ -0,0 +1,14 @@
+//
+//  PlannerDetailView.swift
+//  GoRightNow
+//
+//  Created by WOO on 2022/05/25.
+//
+
+import SwiftUI
+
+struct PlannerDetailView: View {
+    var body: some View {
+        Text("test")
+    }
+}
